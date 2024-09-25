37995ce779c5364416ca4e3566c728105340e9b2
Azamat Zulpykhar <zlpkhr@icloud.com>
2024-09-25 22:12:14 +0200

    Setted up telegram ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Suspense, lazy } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { SDKProvider } from "@telegram-apps/sdk-react";
+import { AppRoot } from "@telegram-apps/telegram-ui";
 
 // Works also with SSR as expected
 const Card = lazy(() => import("./Card"));
@@ -9,23 +10,25 @@ const Card = lazy(() => import("./Card"));
 function App() {
   return (
     <SDKProvider acceptCustomStyles debug>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <AppRoot>
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://reactjs.org" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
 
-      <Suspense fallback={<p>Loading card component...</p>}>
-        <Card />
-      </Suspense>
+        <Suspense fallback={<p>Loading card component...</p>}>
+          <Card />
+        </Suspense>
 
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+      </AppRoot>
     </SDKProvider>
   );
 }
