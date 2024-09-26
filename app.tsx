ea594baf633e448c76ef0c5c0adbc958926d46d2
Azamat Zulpykhar <zlpkhr@icloud.com>
2024-09-26 12:40:45 +0200

    Removed unnecessary effects

--- app.tsx
+++ app.tsx
@@ -1,6 +1,5 @@
 import { Payments } from "#apps/payments";
 import "#packages/tma";
-import { createEvent, targetOrigin } from "#packages/tma";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
@@ -9,9 +8,6 @@ const container = document.querySelector("#app");
 if (!container) {
   throw new Error("createRoot(...): Target container is not a DOM element.");
 }
-
-window.parent.postMessage(createEvent("web_app_ready"), targetOrigin);
-window.parent.postMessage(createEvent("web_app_expand"), targetOrigin);
 
 createRoot(container).render(
   <StrictMode>
